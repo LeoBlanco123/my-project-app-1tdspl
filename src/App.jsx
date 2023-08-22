2477454d2ccfcfdbc482f9ad88264a28c61fe541
LeoBlanco123 <rm99119@fiap.com.br>
2023-08-22 09:18:06 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
   return (
 
     <>
-      <h1>Firts DEPLOY into VERCEL</h1>
+      <h1>First DEPLOY into VERCEL</h1>
       <h2>Vite + React</h2>
       <p>Coded By - rm99119</p>
     </>
